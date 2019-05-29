c27c446c3cb673cb7cce6db42c6e7734160e2e37
Poly000 <1348292515@qq.com>
2019-05-29 22:12:12 +0800

    优化

--- add.h
+++ add.h
@@ -8,7 +8,7 @@ void add(int *p,char input) {
 				for(j=0; j<3; j++) {
 					k=p+i+4*j;
 					if(*k==*(k+4)) {
-						*k*=2;
+						*k<<=1;
 						*(k+4)=0;
 					}
 				}
@@ -20,7 +20,7 @@ void add(int *p,char input) {
 				for(j=2; j>-1; j--) {
 					k=p+i+4*j;
 					if(*k==*(k+4)) {
-						*(k+4)*=2;
+						*(k+4)<<=1;
 						*k=0;
 					}
 				}
@@ -32,7 +32,7 @@ void add(int *p,char input) {
 				for(j=0; j<3; j++) {
 					k=p+i*4+j;
 					if(*k==*(k+1)) {
-						*k*=2;
+						*k<<=1;
 						*(k+1)=0;
 					}
 				}
@@ -44,7 +44,7 @@ void add(int *p,char input) {
 				for(j=2; j>-1; j--) {
 					k=p+i*4+j;
 					if(*k==*(k+1)) {
-						*(k+1)*=2;
+						*(k+1)<<=1;
 						*k=0;
 					}
 				}
