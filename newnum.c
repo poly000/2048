808891657d67de46d53e26f60adf902524c8e687
poly000 <34085039+poly000@users.noreply.github.com>
2019-08-05 15:37:23 +0800

    Update newnum.c

--- newnum.c
+++ newnum.c
@@ -1,9 +1,8 @@
 void newnum(int *p) {
 	srand(time(0));
 	int i;
-	do {
-		i = rand()%16;
-	} while(*(p+i)!=0);
+	do i = rand()%16;
+	while(*(p+i)!=0);
 	if (rand()%10==0) // 约1/10几率新的数为4，否则为2
 		*(p+i) = 4;
 	else
