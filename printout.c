09fc10527913071f6313be21934b2892e4af7c82
poly000 <34085039+poly000@users.noreply.github.com>
2019-08-14 12:12:06 +0800

    emnm

--- printout.c
+++ printout.c
@@ -1,7 +1,11 @@
 void printout(int *p) {
 	register int i,j=0;
 	system("cls");
-	fprintf(stderr,"use R to reset\nuse Q to exit\nuse L to load savedata\nuse O to save savedata\n----------------------------\n\n");
+	fprintf(stderr,"use R to reset\n"
+		       "use Q to exit\n"
+		       "use L to load savedata\n"
+		       "use O to save savedata\n"
+		       "----------------------------\n\n");
 	for(i=0; i<16; i++) {
 		if (*(p+i) != 0)
 			printf(" %5d",*(p+i));
