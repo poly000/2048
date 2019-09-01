283ea76b4f4aa413848ae7a2551b8dbb4574d7f0
poly000 <1348292515@qq.com>
2019-09-01 20:11:54 +0800

    emmm

--- 2048.c
+++ 2048.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <conio.h>
-
-#include "movnum.c"
-#include "add.c"
-#include "newnum.c"
-#include "printout.c"
-
+#include <2048.h>
 
 int main() {
 	time_t now = time(0);
---
+++ 2048.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "movnum.c"
+#include "add.c"
+#include "newnum.c"
+#include "printout.c"
